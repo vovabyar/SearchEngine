072a0584f58100dbb6265b8c92106d148f748cd5
Vladimir Berinchik <53911534+vovabyar@users.noreply.github.com>
2023-08-09 09:40:42 +0300

    Update VideosManager.cpp

--- VideosManager.cpp
+++ VideosManager.cpp
@@ -5,20 +5,18 @@
 
 using json = nlohmann::json;
 
-// Определение структуры Videos
 struct Videos {
-    int id; // Добавляем поле id как индекс
+    int id; 
     std::string title;
     std::string author;
     int duration;
     int views;
 
-    // Метод для сериализации в JSON
+    // for serialization
     void to_json(json& j) const {
         j = json{{"id", id}, {"title", title}, {"author", author}, {"duration", duration}, {"views", views}};
     }
 
-    // Метод для десериализации из JSON
     void from_json(const json& j) {
         if (j.find("id") != j.end()) {
             j.at("id").get_to(id);
@@ -32,10 +30,9 @@ struct Videos {
 
 class VideosManager {
 public:
-    // Создать структуру Videos и заполнить данными, присваивая уникальный id
     Videos createVideo(const std::string& title, const std::string& author, int duration, int views) {
         Videos video;
-        video.id = getNextUniqueId(); // Получаем уникальный id
+        video.id = getNextUniqueId();
         video.title = title;
         video.author = author;
         video.duration = duration;
@@ -43,12 +40,10 @@ public:
         return video;
     }
 
-    // Добавить структуру Videos в коллекцию
     void addVideo(const Videos& video) {
         videos.push_back(video);
     }
 
-    // Сохранить коллекцию Videos в файл JSON
     void saveToJson(const std::string& filename) {
         json jsonData;
 
@@ -62,7 +57,7 @@ public:
 
         std::ofstream outputFile(filePath);
         if (outputFile.is_open()) {
-            outputFile << jsonData.dump(4); // Записываем JSON с отступами (4 пробела)
+            outputFile << jsonData.dump(4); 
             outputFile.close();
             std::cout << "Файл обновлен по пути: " << filePath << std::endl;
         } else {
@@ -153,19 +148,6 @@ private:
 
 int main() {
     VideosManager manager;
-
-//    // Создаем и заполняем структуры Videos
-//    Videos video1 = manager.createVideo("Video 1", "Author 1", 120, 1000);
-//    Videos video2 = manager.createVideo("Video 2", "Author 2", 20, 91000);
-//
-//    // Добавляем структуры Videos в коллекцию
-//    manager.addVideo(video1);
-//    manager.addVideo(video2);
-//
-//    // Сохраняем коллекцию Videos в файл JSON
-//    manager.saveToJson("videos.json");
-
-    // Загружаем коллекцию Videos из файла JSON
     manager.loadFromJson("videos.json");
 
     // Populate the Trie with video titles
@@ -186,49 +168,5 @@ int main() {
     } else {
         std::cerr << "Не удалось открыть файл для записи результатов поиска." << std::endl;
     }
-    // Выводим загруженные видео
-//    std::cout << "Загруженные видео:" << std::endl;
-//    const std::vector<Videos>& loadedVideos = manager.getVideos();
-//    for (const Videos& loadedVideo : loadedVideos) {
-//        std::cout << "ID: " << loadedVideo.id << std::endl;
-//        std::cout << "Название: " << loadedVideo.title << std::endl;
-//        std::cout << "Автор: " << loadedVideo.author << std::endl;
-//        std::cout << "Продолжительность: " << loadedVideo.duration << " сек" << std::endl;
-//        std::cout << "Просмотры: " << loadedVideo.views << std::endl;
-//        std::cout << std::endl;
-//    }
-
     return 0;
 }
-
-
-/*
- * Генерация входных данных.
- * */
-//int main() {
-//    std::ifstream moviesFile("/home/vovabyar/CLionProjects/trie/resources/movies.txt");
-//    if (!moviesFile.is_open()) {
-//        std::cerr << "Не удалось открыть файл 'movies.txt'." << std::endl;
-//        return 1;
-//    }
-//    VideosManager manager;
-//
-//    std::vector<std::string> videoTitles;
-//    std::string line;
-//    while (std::getline(moviesFile, line)) {
-//        videoTitles.push_back(line);
-//    }
-//    moviesFile.close();
-//
-//    json jsonData;
-//
-//    for (int id = 0; id < videoTitles.size(); ++id) {
-//         Videos video = manager.createVideo(videoTitles[id], "Author " + std::to_string(id + 1), rand() % 300 + 60, rand() % 10000000);
-//         manager.addVideo(video);
-//
-//    }
-//
-//    manager.saveToJson("videos.json");
-//
-//    return 0;
-//}
